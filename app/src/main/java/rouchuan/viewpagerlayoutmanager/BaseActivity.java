eb2ebfe45adc1c89fc29bdca62463fd9f2ddd435
jbs-fm <36034436+jbs-fm@users.noreply.github.com>
2019-10-29 11:16:05 +0000

    Migrate to android x

--- app/src/main/java/rouchuan/viewpagerlayoutmanager/BaseActivity.java
+++ app/src/main/java/rouchuan/viewpagerlayoutmanager/BaseActivity.java
@@ -1,10 +1,10 @@
 package rouchuan.viewpagerlayoutmanager;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.graphics.drawable.VectorDrawableCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.Nullable;
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
--- viewpagerlayoutmanager/src/main/java/com/leochuan/AutoPlaySnapHelper.java
+++ viewpagerlayoutmanager/src/main/java/com/leochuan/AutoPlaySnapHelper.java
@@ -2,8 +2,8 @@ package com.leochuan;
 
 import android.os.Handler;
 import android.os.Looper;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.Scroller;
 
